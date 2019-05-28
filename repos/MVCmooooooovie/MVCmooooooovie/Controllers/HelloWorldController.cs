907feb11c8e0c9b12164ecffe06758ee35cc9258
FrozenBeans <guillermoetuyub@yahoo.com>
2019-05-27 22:53:23 -0500

    attempt 2 from laptop

---
+++ repos/MVCmooooooovie/MVCmooooooovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCmooooooovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        public string Math(int num1 = 1, int num2 = 2)
+        {
+            return HtmlEncoder.Default.Encode($"{num1 + num2}");
+        }
+    }
+}
---
+++ repos/SQLandEF/ADOApplication/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data; //brings libraries required for applications connected to database
+using System.Data.SqlClient; //for SQL
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = "Data Source = gt-dbserver.database.windows.net; Initial Catalog = AdventureWorks; Persist Security Info = True;"+
+            "User ID = guillermoT; Password = Kratos0909!";
+            string query = "select * from SalesLT.Customer";
+
+            //1. Make a sql connection
+            SqlConnection con;
+            con = new SqlConnection(conStr); //constr is endpoint
+            con.Open();
+            //2. fire query
+
+            SqlCommand cmd;
+            cmd = new SqlCommand(query, con);
+            //3. execute query and get results;
+            SqlDataReader dr;
+            dr=cmd.ExecuteReader(); //used for select commands can use cmd.executenonquery() - which are used for insert/update/delete
+            //unmanaged data
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Console.WriteLine($"Id: {dr[0]} name {dr["FirstName"]} {dr["LastName"]} {dr["MiddleName"]}");
+                }//read pulls one record at a time. cannot backtrack- why we should read/store previous records
+            }
+            con.Close();
+        }
+    }
+}
